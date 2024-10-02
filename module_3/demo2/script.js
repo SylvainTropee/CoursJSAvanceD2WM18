ba962b74e6ba0475aa64d44857279807881b4af8
stropee <stropee@campus-eni.fr>
2024-10-02 13:51:10 +0200

    Module 4 demo api

--- module_3/demo2/script.js
+++ module_3/demo2/script.js
@@ -1,6 +1,19 @@
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+let pokemon;
+
+//lancement de l'appel asynchrone
+ let f = fetch("https://pokeapi.co/api/v2/pokemon/ditto"/*, {method : "POST", body : JSON.stringify({name : "Robert"})}*/)
+    //fetch me renvoie une promise qui contient la reponse HTTP
+    //j'en extraie la partie json, then() me permet de résoudre la promise
+    //cela lui indique quoi faire quand l'appel revient
     .then(response => response.json())
+    //extraire la partie JSON prend du temps, cela me renvoie une nouvelle
+    //promise qui contient uniquement mes données
     .then(data => {
+
+        pokemon = data
+        console.log("on est là", pokemon);
+        //je traite mes données
+        //et génère la vue à partir de celle-ci
         console.log(data)
         let h1 = document.createElement('h1')
         h1.innerText = data.name.toUpperCase()
@@ -8,7 +21,9 @@ fetch("https://pokeapi.co/api/v2/pokemon/ditto")
         let img = document.createElement('img')
         img.src = data.sprites.other.home.front_default
 
-
         document.body.append(h1)
         document.body.append(img)
     })
+
+console.log(f)
+console.log("on est ici", pokemon);
